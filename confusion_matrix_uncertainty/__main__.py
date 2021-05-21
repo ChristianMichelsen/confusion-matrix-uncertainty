99137c951cfbef57a715d9fdcd47274868a11336
Christian Michelsen <christianmichelsen@gmail.com>
2021-05-21 11:00:57 +0200

    First commit

---
+++ confusion_matrix_uncertainty/__main__.py
@@ -0,0 +1,2 @@
+from .main import app
+app(prog_name="confusion-matrix-uncertainty")
---
+++ tests/test_confusion_matrix_uncertainty.py
@@ -0,0 +1,5 @@
+from confusion_matrix_uncertainty import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
